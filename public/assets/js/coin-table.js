f7a3611bfc7c138aed1b7ea17ba0e433bc220d69
Sude Önder <sudeonder999@gmail.com>
2024-05-25 12:08:34 +0300

    add chart

--- public/assets/js/coin-table.js
+++ public/assets/js/coin-table.js
@@ -3,10 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.json())
     .then((data) => populateTable(data))
     .catch((error) => console.error("Error fetching data:", error));
-
 });
-
-// TODO - refactor
 
 function populateTable(data) {
   const tableBody = document.getElementById("coin-table");
@@ -26,13 +23,13 @@ function populateTable(data) {
       // Create and append coin logo image
       const logoCell = document.createElement("td");
       if (crypto.logo) {
-        const logoImg = document.createElement('img');
+        const logoImg = document.createElement("img");
         logoImg.src = crypto.logo;
         logoImg.alt = crypto.name;
-        logoImg.classList.add('avatar');
+        logoImg.classList.add("avatar");
         logoCell.appendChild(logoImg);
-        }
-       row.appendChild(logoCell);
+      }
+      row.appendChild(logoCell);
 
       const priceCell = document.createElement("td");
       priceCell.textContent = crypto.price ? crypto.price.toFixed(2) : "-";
@@ -108,12 +105,54 @@ function populateTable(data) {
       row.appendChild(marketCapCell);
 
       const volume24h = document.createElement("td");
-      volume24h.textContent = crypto.market_cap
+      volume24h.textContent = crypto.volume_24h
         ? crypto.volume_24h.toLocaleString("en")
         : "-";
       row.appendChild(volume24h);
 
+      // Add the graph cell
+      const graphCell = document.createElement("td");
+      const graphDiv = document.createElement("div");
+      graphDiv.id = `chart-${crypto.id}`; // Unique ID for each chart
+      graphCell.appendChild(graphDiv);
+      row.appendChild(graphCell);
+
       tableBody.appendChild(row);
+
+      // Initialize the ApexChart
+      var options = {
+        series: [{
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+        chart: {
+        width:250,
+        height: 180,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      }
+      };
+      
+
+      const chart = new ApexCharts(document.querySelector(`#chart-${crypto.id}`), options);
+      chart.render();
     }
   }
 }
